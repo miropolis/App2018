49c5244a5248e08cc79f53012a918ea03dc6d974
Miro We <39390507+Wyrda1@users.noreply.github.com>
2018-06-26 15:59:42 +0200

    Added RootPage conditional

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,15 +8,31 @@ import { Storage } from '@ionic/storage';
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = 'LoginPage';
+  rootPage:any
 
   constructor(platform: Platform, storage: Storage, statusBar: StatusBar, splashScreen: SplashScreen) {
+    storage.get("isLoggedIn").then((status) => {
+      console.log("component logged in?", status)
+      if(status) {
+        this.goToTabsPage()
+      } else{
+        this.goToLoginPage()
+      }
+    });
+
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
     });
+  }
+
+ goToTabsPage ( ) {
+    this.rootPage = "TabsPage"
+  }
+  goToLoginPage() {
+    this.rootPage = "LoginPage"
   }
 }
 
