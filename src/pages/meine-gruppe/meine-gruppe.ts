1f02d0f3f8db3681cd2e76fdb96bc491eae9a495
Miro We <39390507+Wyrda1@users.noreply.github.com>
2018-07-02 16:52:07 +0200

    fixed group functionality

--- src/pages/meine-gruppe/meine-gruppe.ts
+++ src/pages/meine-gruppe/meine-gruppe.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
 
 /**
  * Generated class for the MeineGruppePage page.
@@ -14,12 +15,47 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'meine-gruppe.html',
 })
 export class MeineGruppePage {
+GruppeNummer;
+Gruppe1;
+Gruppe2;
+Gruppe3;
+Gruppe4;
+Gruppe5;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+
+  constructor(public navCtrl: NavController, private storage: Storage, public navParams: NavParams) {
+    this.storage.get('group').then((val) => {
+      switch(val) {
+        case 1:
+            this.Gruppe1=true;
+            break;
+        case 2:
+            this.Gruppe2=true;
+            break;
+        case 3:
+            this.Gruppe3=true;
+            break;
+        case 4:
+            this.Gruppe4=true;
+            break;
+        case 5:
+            this.Gruppe5=true;
+            break;
+      
+        } 
+    });
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad MeineGruppePage');
+    /*let element = document.getElementById("g1");
+    element.style.display = "none";*/
+  }
+
+
+
+  displayGroup(){
+    console.log('Gruppen Nummer:', this.GruppeNummer);
   }
 
 }
